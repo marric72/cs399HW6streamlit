d0c06af2e680d3c036491934cd3e90a75fefbd67
Heather Marriott <heathermarriott1@gmail.com>
2024-03-06 11:59:46 -0700

    new images

--- curriculumMap.py
+++ curriculumMap.py
@@ -10,12 +10,12 @@ title = '<p style="font-family:Courier; color:Blue; font-size: 30px;">CYBOT - Cy
 st.markdown(title, unsafe_allow_html=True)
 
 #write user input to screen
-n=st.text_input("Enter name")
-if len(n) > 0:
-    st.write(f"Hello {n}")
+#n=st.text_input("Enter name")
+#if len(n) > 0:
+#    st.write(f"Hello {n}")
 
 
-is_clicked = st.button("Click Me")
+#is_clicked = st.button("Click Me")
 
 #st.image("finalPanda.png", caption=' ')
 
@@ -39,46 +39,22 @@ st.write(scenario)
 ##                 'Sophomore, from Denver, majoring in Software Engineering, third-generation Chinese immigrant, has some knowledge of Chinese culture, enjoys social media. Hope to work as a software engineer in the future.',
 ##                 'Junior student, from Houston. Cybersecurity major, enjoys cooking and traveling. Hope to work in cybersecurity in the future.']
 ## 
-data = {
 
+data = {
     'Column 1': ['Melissa 周萌', 'Jaden 王杰丹', 'Mei  李梅', 'Diego 张笛'],
-    'Column 2': ['Freshman from Los Angela California; Asian Studies major',
+    'Column 2': ['Freshman from Los Angela California; Asian Studies major, extroverted; likes computer games; Desired work related to Asian Studies. ',
                  'Junior student from New York. Chinese major. Enjoys playing basketball and singing. Hopes to work in China in the future.',
-                 'Sophomore, from Denver, majoring in Software Engineering, third-generation Chinese immigrant, ',
+                 'Female, sophomore, from Denver, majoring in Software Engineering, third-generation Chinese immigrant, has some knowledge of Chinese culture, enjoys social media. Hope to work as a software engineer in the future.',
                  'Junior student, from Houston. Cybersecurity major, enjoys cooking and traveling. Hope to work in cybersecurity in the future.'],
-     'Column 3': ['panda.png', 'panda.png', 'panda.png', 'panda.png']
+     'Column 3': ['Melissa.png', 'Jade.png', 'Mei.png', 'Diego.png']
 }
 df = pd.DataFrame(data)
-# Define CSS style for the table
-##table_style = """
-##    <style>
-##        table {
-##            border-collapse: collapse;
-##        }
-##        th, td {
-##            border: 1px solid black;
-##            padding: 8px;
-##            text-align: left;
-##        }
-##    </style>
-##"""
-##
-### Display the HTML table with borders
-##st.write(table_style, unsafe_allow_html=True)
-##st.write(df)
-
-# Display the table with borders
-##st.markdown(
-##    df.style.set_properties(**{'border': '1px solid black', 'border-collapse': 'collapse'}).render(), 
-##    unsafe_allow_html=True
-##)
-# Display the table with images
+# Display the table with images and text side by side
 for index, row in df.iterrows():
-    col1, col2, col3 = st.columns([1, 3, 1])  # Define column widths
+    st.write("---")
+    col1, col2 = st.columns([1, 3])  # Define column widths
     with col1:
-        st.write(f"**{row['Column 1']}**")
+        st.image(row['Column 3'], width=150, caption=row['Column 1'])
     with col2:
+        st.write(f"**{row['Column 1']}**")
         st.write(row['Column 2'])
-    with col3:
-        st.image(row['Column 3'], width=200, caption=row['Column 1'])
-##
