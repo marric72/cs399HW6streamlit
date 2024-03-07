a2c3596798d52b1186041f84550a82fa4bda373a
Heather Marriott <heathermarriott1@gmail.com>
2024-03-07 12:30:17 -0700

    other pages

--- pages/6_episode_5.py
+++ pages/6_episode_5.py
@@ -3,6 +3,6 @@ import pandas as pd
 import numpy as np
 import base64
 
-title = '<p style="font-family:Courier; color:Blue; font-size: 30px;">Episode 2 - Coming Soon</p>'
+title = '<p style="font-family:Courier; color:Blue; font-size: 30px;">Episode 5 - Coming Soon</p>'
 st.markdown(title, unsafe_allow_html=True)
 st.image("panda.png", caption=' ')
